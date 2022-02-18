e7e70c7d7848dbc7d1c1deb3c66b82656a955368
tayfundagci <tayfundagci3517@gmail.com>
2022-02-18 16:47:45 +0300

    add nightmode contex

--- client/src/pages/Admin/Home/index.js
+++ client/src/pages/Admin/Home/index.js
@@ -1,7 +1,12 @@
+import { Box } from "@chakra-ui/react";
 import React from "react";
 
 function Home() {
-  return <div>Admin Home</div>;
+  return (
+    <div>
+      <Box textAlign="center" fontSize="2xl"></Box>
+    </div>
+  );
 }
 
 export default Home;
